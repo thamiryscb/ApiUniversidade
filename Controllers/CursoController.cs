3dd60783541b6f5d49a82a0c47f46bba14a99937
Thamirys <thamirys.barbosa@escolar.ifrn.edu.br>
2023-10-10 11:49:13 -0300

    ALTERAÇÕES

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apiUniversidade.Context;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +12,8 @@ namespace apiUniversidade.Controllers
     [Route("[controller]")]
     public class CursoController : ControllerBase
     {
-        private reandoly ILoger<CursoController> _logger;
-        private reandoly apiUniversidadeContext _context;
+        private readonly ILogger<CursoController> _logger;
+        private readonly ApiUniversidadeContext _context;
         public CursoController(ILogger<CursoController> logger, ApiUniversidadeContext context)
         {
             _logger = logger;
@@ -21,7 +22,7 @@ namespace apiUniversidade.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Curso>> Get()
         {
-            var cursos = context.Cursos.ToList();
+            var cursos = _context.Cursos.ToList();
             if(cursos is null)
                 return NotFound();
 
