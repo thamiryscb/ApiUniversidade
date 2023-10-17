eca2cf974c058d5d09457bf00211cbb225472c2f
Thamirys <thamirys.barbosa@escolar.ifrn.edu.br>
2023-10-17 11:54:19 -0300

    fomee

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -28,5 +28,14 @@ namespace apiUniversidade.Controllers
 
             return cursos;
         }
+        [HttpPost]
+        public ActionResult Post(Curso curso){
+            _context.Cursos.Add(curso);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetCurso",
+                new{ id = curso.Id},
+                curso); 
+        }
     }
 }
