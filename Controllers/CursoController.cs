96ca98836765cec0bcbb10a9fcdc43570a660269
Thamirys <thamirys.barbosa@escolar.ifrn.edu.br>
2023-10-24 11:48:10 -0300

    criação de Put e Post

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -28,6 +28,17 @@ namespace apiUniversidade.Controllers
 
             return cursos;
         }
+
+        [HttpGet("{id:int}", Name="GetCurso")]
+        public ActionResult<Curso> Get(int id)
+        {
+            var curso = _context.Cursos.FirstOrDefault(p => p.Id == id);
+            if(curso is null)
+                return NotFound("Curso não encontrado.");
+            
+            return curso;
+        }
+
         [HttpPost]
         public ActionResult Post(Curso curso){
             _context.Cursos.Add(curso);
