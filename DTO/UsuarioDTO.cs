1d4a92fb2161b161e3e93d1b8026de34d9b022a2
Thamirys <thamirys.barbosa@escolar.ifrn.edu.br>
2023-11-28 11:50:43 -0300

    Alteração 3 28/11

---
+++ DTO/UsuarioDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiUniversidade.DTO;
+public class UsusarioDTO
+{
+    public string Email {get; set;}
+    public string Password {get; set;}
+    public string ConfirmPassword {get; set;}
+}
---
+++ DTO/UsuarioToken.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiUniversidade.DTO;
+public class UsuarioToken
+{
+    public bool Authenticated {get; set;}
+    public DateTime Expiration {get; set;}
+    public string Token {get; set;}
+    public string Message {get; set;}
+}
