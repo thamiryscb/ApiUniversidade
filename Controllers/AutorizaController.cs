1eea1a67ccf393fc303b96b8db6d7431ffb3c336
Thamirys <thamirys.barbosa@escolar.ifrn.edu.br>
2023-12-05 10:10:26 -0300

    métodos

--- Controllers/AutorizaController.cs
+++ Controllers/AutorizaController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiUniversidade.DTO;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,5 +21,27 @@ namespace ApiUniversidade.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [HttpGet]
+            public ActionResult<string> Get(){
+                return "AutorizaController :: Acessado em: " + DateTime.Now.ToLongDateString();
+            }
+
+        [HttpPost("Register")]
+            public async Task<ActionResult> RegisterUser([FromBody]UsuarioDTO model){
+                var user = new IdentityUser{
+                    UserName = model.Email,
+                    Email = model.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if(!result.Succeeded)
+                    return BadRequest(result.Errors);
+
+                await _signInManager.SignInAsync(user, false);
+                //return Ok(GerarToken(model));
+                return Ok();
+            }
     }
 }
--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ApiUniversidade.DTO;
-public class UsusarioDTO
+public class UsuarioDTO
 {
     public string Email {get; set;}
     public string Password {get; set;}
